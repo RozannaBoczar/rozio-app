bd8252dced7ba41151907bf9268a92d217ab736c
RozannaBoczar <rozanna.boczar@gmail.com>
2025-07-05 13:05:04 +0200

    add server setup and update start script in package.json

---
+++ server.js
@@ -0,0 +1,17 @@
+// server.js
+const express = require('express');
+const path = require('path');
+const app = express();
+
+const PORT = process.env.PORT || 8080;
+
+// Zmień 'your-app-name' na folder z dist/ z angular.json
+app.use(express.static(path.join(__dirname, 'dist/rozio-app')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/rozio-app/src/index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
